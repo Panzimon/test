02a7f3fa3a500d0fbe2a210a70b577a013b790d3
Promise.zi <377578872@qq.com>
2017-01-19 15:43:29 +0800

    closure

--- Closure.js
+++ Closure.js
@@ -76,3 +76,62 @@ var c2 = create_counter(10);
 c2.inc(); // 11
 c2.inc(); // 12
 c2.inc(); // 13
+
+function make_pow(n) {
+    return function (x) {
+        return Math.pow(x, n);
+    }
+}
+console.log(make_pow(4)(4));
+
+
+// 定义数字0:
+var zero = function (f) {
+    return function (x) {
+        return x;
+    }
+};
+
+// 定义数字1:
+var one = function (f) {
+    return function (x) {
+        return f(x);
+    }
+};
+
+// 定义加法:
+function add(n, m) {
+    return function (f) {
+        return function (x) {
+            return m(f)(n(f)(x));
+        }
+    }
+}
+
+// 计算数字2 = 1 + 1:
+var two = add(one, one);
+
+// 计算数字3 = 1 + 2:
+var three = add(one, two);
+
+// 计算数字5 = 2 + 3:
+var five = add(two, three);
+
+// 你说它是3就是3，你说它是5就是5，你怎么证明？
+
+// 呵呵，看这里:
+
+// 给3传一个函数,会打印3次:
+(three(function () {
+    console.log('print 3 times');
+}))();
+
+// 给5传一个函数,会打印5次:
+(five(function () {
+    console.log('print 5 times');
+}))();
+
+// 继续接着玩一会...
+
+
+
