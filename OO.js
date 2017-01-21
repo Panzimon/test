c48d4ff46f23df2c230a08e7860c65508cd25818
Promise.zi <377578872@qq.com>
2017-01-21 23:15:47 +0800

    _proto_

--- OO.js
+++ OO.js
@@ -1,3 +1,24 @@
 /**
  * Created by ZY on 2017/1/21.
  */
+//在JavaScript中，这个概念需要改一改。JavaScript不区分类和实例的概念，
+// 而是通过原型（prototype）来实现面向对象编程。
+
+var stu = {
+  name : 'SB',
+  height :180,
+  run :function (speed) {
+      console.log(this.name +
+          ' is running at the speed of ' + speed +' km/D');
+  }
+  //...
+};
+
+var Xiaoming = {
+    name : 'XM',
+};
+
+Xiaoming.__proto__ = stu;//_proto_不等于prototype！
+
+console.log(Xiaoming);
+Xiaoming.run(5);
