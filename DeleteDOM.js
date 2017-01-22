7995927ed42f277ee86e2ae78e7ac72cee0533e1
Promise.zi <377578872@qq.com>
2017-01-22 23:14:35 +0800

    删除后的节点虽然不在文档树中了，  但其实它还在内存中，可以随时再次被添加到别的位置

--- DeleteDOM.js
+++ DeleteDOM.js
@@ -1,3 +1,18 @@
 /**
  * Created by XYSM on 2017/1/22.
  */
+
+//removeChild
+/*
+ // 拿到待删除节点:
+ var self = document.getElementById('to-be-removed');
+ // 拿到父节点:
+ var parent = self.parentElement;
+ // 删除:
+ var removed = parent.removeChild(self);
+ removed === self; // true
+
+ p.s.
+ 注意到删除后的节点虽然不在文档树中了，
+ 但其实它还在内存中，可以随时再次被添加到别的位置
+ */
