6ea7b651f20e65122eee5c247378d8b8cce4dcd4
Promise.zi <377578872@qq.com>
2017-01-22 14:42:15 +0800

    class end

--- classInherit.js
+++ classInherit.js
@@ -25,3 +25,11 @@ class PrimaryStudent extends Student{
         console.log('I am at grade '+ this.grade);
     }
 }
+//class的好处就是极大地简化了原型链代码
+
+/*
+ 不是所有的主流浏览器都支持ES6的class。如果一定要现在就用上，
+ 就需要一个工具把class代码转换为传统的prototype代码，
+ 可以试试Babel这个工具
+ */
+
