31b72b7e1d3857a3a4fa806febb1972721667eb0
Promise.zi <377578872@qq.com>
2017-01-22 12:20:44 +0800

    =。=

--- classInherit.js
+++ classInherit.js
@@ -1,3 +1,12 @@
 /**
  * Created by ZY on 2017/1/22.
  */
+
+class Student {
+    constructor(name){
+        this.name = name;
+    }
+    hello(){
+        console.log('Hello, ' + this.name + '!');
+    }
+}
