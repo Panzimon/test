7823039dcd5a2efb8eeff6a2c9554fdd69977a9b
Promise.zi <377578872@qq.com>
2017-01-23 17:05:57 +0800

    为什么超过1s的都是reject。。。

--- Promise.js
+++ Promise.js
@@ -21,3 +21,27 @@ request.onreadystatechange = function () {
     }
 }
 */
+
+function test(resolve,reject){
+    'use strict'
+    let timeOut = Math.random() * 2 ;
+    console.log('set timeout to: '+timeOut.toFixed(3)+' seconds.');
+    setTimeout(function () {
+        if(timeOut < 1 ){
+            console.log('call resolve()...')
+            resolve('200 OK');
+        }
+        else{
+            console.log('call reject()...');
+            reject('timeout in '+timeOut +' seconds. ');
+        }
+    },timeOut * 1000);
+}
+
+var p1 = new Promise(test);
+var p2 = p1.then(function (result) {
+    console.log('成功：'+ result);
+});
+var p3 = p2.catch(function (reason) {
+    console.log('失败：'+reason);
+});
