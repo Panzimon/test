a0a14d08320d49bb309184a71d3fb857bc109f1f
Promise.zi <377578872@qq.com>
2017-01-23 22:01:27 +0800

    puzzled canvas

--- Canvas.js
+++ Canvas.js
@@ -1,3 +1,48 @@
 /**
  * Created by XYSM on 2017/1/23.
  */
+//由于浏览器对HTML5标准支持不一致，所以，通常在<canvas>内部添加一些说明性HTML代码，如果浏览器支持Canvas，它将忽略<canvas>内部的HTML，如果浏览器不支持Canvas，它将显示<canvas>内部的HTML：
+/*
+<canvas id="test-stock" width="300" height="200">
+    <p>Current Price: 25.51</p>
+</canvas>
+*/
+/*
+ getContext('2d')方法让我们拿到一个CanvasRenderingContext2D对象，所有的绘图操作都需要通过这个对象完成。
+
+ var ctx = canvas.getContext('2d');
+ 如果需要绘制3D怎么办？HTML5还有一个WebGL规范，允许在Canvas中绘制3D图形：
+
+ gl = canvas.getContext("webgl");
+ */
+
+/*
+ Canvas的坐标以左上角为原点，水平向右为X轴，
+ 垂直向下为Y轴，以像素为单位，所以每个点都是非负整数。
+
+
+ */
+/*
+圆形
+ 'use strict';
+
+ var
+ canvas = document.getElementById('test-shape-canvas'),
+ ctx = canvas.getContext('2d');
+
+ ctx.clearRect(0, 0, 200, 200); // 擦除(0,0)位置大小为200x200的矩形，擦除的意思是把该区域变为透明
+ ctx.fillStyle = '#dddddd'; // 设置颜色
+ ctx.fillRect(10, 10, 130, 130); // 把(10,10)位置大小为130x130的矩形涂色
+ // 利用Path绘制复杂路径:
+ var path=new Path2D();
+ path.arc(75, 75, 50, 0, Math.PI*2, true);
+ path.moveTo(110,75);
+ path.arc(75, 75, 35, 0, Math.PI, false);
+ path.moveTo(65, 65);
+ path.arc(60, 65, 5, 0, Math.PI*2, true);
+ path.moveTo(95, 65);
+ path.arc(90, 65, 5, 0, Math.PI*2, true);
+ ctx.strokeStyle = '#0000ff';
+ ctx.stroke(path);
+
+ */
