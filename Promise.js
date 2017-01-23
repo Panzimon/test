3d617c76ec28ef58eaa17485e196a51a61aee3e2
Promise.zi <377578872@qq.com>
2017-01-23 16:51:29 +0800

    Promise init

--- Promise.js
+++ Promise.js
@@ -1,3 +1,23 @@
 /**
  * Created by XYSM on 2017/1/23.
  */
+//终于不用全部打备注了~
+function callback() {
+    console.log('Done');
+}
+console.log('before setTimeout()');
+setTimeout(callback,10);//即使是0也是最后执行
+console.log('after setTimeset()');//其实还是before
+
+/*
+request.onreadystatechange = function () {
+    if(request.readyState === 4){
+        if(request.status === 200){
+            return success(request.responseText);
+        }
+        else {
+            return fail(request.status);
+        }
+    }
+}
+*/
