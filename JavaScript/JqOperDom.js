171bf8674e81bd5e3390cbb6979bc09beac3b0c4
Promise.zi <377578872@qq.com>
2017-01-24 17:36:17 +0800

    prop() attr() checked .is()

--- JavaScript/JqOperDom.js
+++ JavaScript/JqOperDom.js
@@ -139,3 +139,29 @@
  radio.is(':checked'); // true
  类似的属性还有selected，处理时最好用is(':selected')。
  */
+
+/*
+
+ <input id="test-input" name="email" value="">
+ <select id="test-select" name="city">
+ <option value="BJ" selected>Beijing</option>
+ <option value="SH">Shanghai</option>
+ <option value="SZ">Shenzhen</option>
+ </select>
+ <textarea id="test-textarea">Hello</textarea>
+
+var
+    input = $('#test-input'),
+    select = $('#test-select'),
+    textarea = $('#test-textarea');
+
+input.val(); // 'test'
+input.val('abc@example.com');
+// 文本框的内容已变为abc@example.com
+
+select.val(); // 'BJ'
+select.val('SH'); // 选择框已变为Shanghai
+
+textarea.val(); // 'Hello'
+textarea.val('Hi'); // 文本区域已更新为'Hi'
+ */
