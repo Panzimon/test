77cbaf18e0233de8ef1162ad9e7015f24ad4171d
Promise.zi <377578872@qq.com>
2017-01-26 00:05:09 +0800

    BME end

--- Node/BasicModuleExp.js
+++ Node/BasicModuleExp.js
@@ -1,3 +1,34 @@
 /**
  * Created by XYSM on 2017/1/25.
  */
+
+console.log('current js file: ' + __filename);
+
+process.on('exit', function (code) {
+    console.log('about to exit with code: ' + code);
+});
+
+console.log('current js dir: ' + __dirname);
+
+process.name = 'sample node';
+
+console.log(process.name);
+// process.argv 存储了命令行参数:
+console.log('arguments: ' + JSON.stringify(process.argv));
+
+// process.cwd() 返回当前工作目录:
+console.log('cwd: ' + process.cwd());
+
+// 切换当前工作目录:
+var d = '/private/tmp';
+if (process.platform === 'win32') {
+    // 如果是Windows，切换到 C:\Windows\System32
+    d = 'C:\\Windows\\System32';
+}
+process.chdir(d);
+console.log('cwd: ' + process.cwd());
+
+process.nextTick(function () {
+    console.log('nextTick callback!');
+});
+console.log('nextTick was set!');
