3792865f05fec63308cf0fe55ec2264bff8cdb6b
Promise.zi <377578872@qq.com>
2017-01-25 21:27:01 +0800

    _Chaining end

--- JavaScript/_Chaining.js
+++ JavaScript/_Chaining.js
@@ -1,3 +1,17 @@
 /**
  * Created by XYSM on 2017/1/25.
  */
+var _ = require('underscore');
+
+console.log(_
+    .filter(_
+    .map([1, 4, 9, 16, 25],
+        Math.sqrt), x => x % 2 === 1)
+);
+
+console.log(
+    _.chain([1, 4, 9, 16, 25])
+    .map(Math.sqrt)
+    .filter(x => x % 2 === 0)
+    .value()
+);
