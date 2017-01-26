10a64b389aa0df75d2917e8a8b7152c7767321a6
Promise.zi <377578872@qq.com>
2017-01-27 00:57:49 +0800

    Node_http init

--- Node/Node_http.js
+++ Node/Node_http.js
@@ -1,3 +1,11 @@
 /**
  * Created by ZY on 2017/1/27.
  */
+
+/*
+ 要开发HTTP服务器程序，
+ 从头处理TCP连接，解析HTTP是不现实的。
+ 这些工作实际上已经由Node.js自带的http模块完成了。应用程序并
+ 不直接和HTTP协议打交道，
+ 而是操作http模块提供的request和response对象。
+ */
