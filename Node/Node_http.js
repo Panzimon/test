e5939b18bd2f6dde2b0566dac8c899862d58d0dd
Promise.zi <377578872@qq.com>
2017-01-27 01:51:05 +0800

    http_server test got

--- Node/Node_http.js
+++ Node/Node_http.js
@@ -9,3 +9,21 @@
  不直接和HTTP协议打交道，
  而是操作http模块提供的request和response对象。
  */
+/*
+request对象封装了HTTP请求，
+我们调用request对象的属性和方法就可以拿到所有HTTP请求的信息；
+
+response对象封装了HTTP响应，
+我们操作response对象的方法，就可以把HTTP响应返回给浏览器。
+*/
+var http = require('http');
+
+var server = http.createServer(function(request,response){
+    console.log(request.method +' : '+request.url);
+    response.writeHead(200,{'Content-Type': 'text/html'});
+    response.end('<h1>Hello world ~<h1>');
+});
+
+server.listen(8050);
+
+console.log('Server is running at http://127.0.0.1:8050');
