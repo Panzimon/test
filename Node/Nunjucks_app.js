afb1745feac3ce03f734c81f2eac108c27d1c794
Promise.zi <377578872@qq.com>
2017-01-29 18:44:36 +0800

    to be 查看文档~~~

--- Node/Nunjucks_app.js
+++ Node/Nunjucks_app.js
@@ -1,3 +1,42 @@
 /**
  * Created by ZY on 2017/1/29.
  */
+
+const nunjucks = require('nunjucks');
+
+function createEnv(path,opts) {
+    var
+        autoscape = opts.autoscape && true ,
+        noCache = opts.noCache || false ,
+        watch = opts.watch || false ,
+        throwOnUnderfined = opts.throwOnUndefined || false ,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views',{
+            noCache: noCache,
+                watch: watch,
+            }),{
+            autoscape: autoscape,
+                throwOnUnderfined: throwOnUnderfined
+            }
+        );
+    if(opts.filters){
+        for(var f in opts.filters){
+            env.addFilter(f,opts.filter[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views',{
+   watch: true,
+    filters: {
+       hex: function (n) {
+           return '0x' + n.toString(16);
+       }
+    }
+});
+/*
+ 变量env就表示Nunjucks模板引擎对象，
+ 它有一个render(view, model)方法，
+ 正好传入view和model两个参数，并返回字符串。
+ */
