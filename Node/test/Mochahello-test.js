88438412f101505c84a47d214a86630b254b48d6
Promise.zi <377578872@qq.com>
2017-02-03 22:50:47 +0800

    before after ~each

--- Node/test/Mochahello-test.js
+++ Node/test/Mochahello-test.js
@@ -1,6 +1,7 @@
 /**
  * Created by ZY on 2017/2/3.
  */
+/*
 const assert = require('assert'),
     sum = require('../Mochahello');
 
@@ -22,7 +23,7 @@ describe('#Mochahello.js',()=>{
            assert.strictEqual(sum(1, 2, 3), 6);
        });
    });
-});
+});*/
 //这里我们使用mocha默认的BDD-style的测试。
 // describe可以任意嵌套，以便把相关测试看成一组测试。
 //每个it("name", function() {...})就代表一个测试。
@@ -30,3 +31,49 @@ describe('#Mochahello.js',()=>{
 // 我们编写多个测试来分别测试不同的输入，
 // 并使用assert判断输出是否是我们所期望的。
 
+//在测试前初始化资源，测试后释放资源是非常常见的。
+// mocha提供了before、after、beforeEach和afterEach来实现这些功能。
+
+const assert = require('assert');
+const sum = require('../Mochahello');
+
+describe('#Mochahello.js', () => {
+    describe('#sum()', () => {
+        before(function () {
+            console.log('before:');
+        });
+
+        after(function () {
+            console.log('after.');
+        });
+
+        beforeEach(function () {
+            console.log('  beforeEach:');
+        });
+
+        afterEach(function () {
+            console.log('  afterEach.');
+        });
+
+        it('sum() should return 0', () => {
+            assert.strictEqual(sum(), 0);
+        });
+
+        it('sum(1) should return 1', () => {
+            assert.strictEqual(sum(1), 1);
+        });
+
+        it('sum(1, 2) should return 3', () => {
+            assert.strictEqual(sum(1, 2), 3);
+        });
+
+        it('sum(1, 2, 3) should return 6', () => {
+            assert.strictEqual(sum(1, 2, 3), 6);
+        });
+    });
+});
+/*
+再次运行，
+可以看到每个test执行前后会分别执行beforeEach()和afterEach()，
+以及一组test执行前后会分别执行before()和after()：
+ */
