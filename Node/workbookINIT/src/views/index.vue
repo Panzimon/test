48ae19dcbc58eb5f4e4af64666452dee42609ade
Promise.zi <377578872@qq.com>
2017-02-26 03:05:32 +0800

    Markdown bug

---
+++ Node/workbookINIT/src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<raw-editor></raw-editor>
+		<render-editor></render-editor>
+	</div>
+</template>
+
+<script>
+	import rawEditor from '../components/rawEditor'
+	import renderEditor from '../components/renderEditor'
+
+	export default {
+		components:{
+			rawEditor,
+			renderEditor
+		}
+	}
+</script>
