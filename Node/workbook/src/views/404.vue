52dbdd1cf98641f1ed07a62bf950d0837b331cd1
Promise.zi <377578872@qq.com>
2017-03-11 01:25:33 +0800

    404

--- Node/workbook/src/views/404.vue
+++ Node/workbook/src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="404">
-  <img src="../assets/logo.png">
-  <h1>404 Not Found</h1>
+    <img class="child" src="../assets/logo.png">
+    <h1 class="child">404 Not Found</h1>
   </div>
 </template>
 
@@ -10,15 +10,3 @@
 
     }
 </script>
-
-
-<style scoped>
-  #404 {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
